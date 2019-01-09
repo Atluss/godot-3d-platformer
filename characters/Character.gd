41e3a9d9095fe6e60642a4d6fca8914152efc846
Shokin Ilya <atlus3@mail.ru>
2019-01-09 20:55:02 +0300

    jump fix

--- characters/Character.gd
+++ characters/Character.gd
@@ -14,8 +14,10 @@ const BUMP_DISTANCE = 4
 const MAX_BUMP_HEIGHT = 1
 const AIR_ACCELERATION = 10
 
-const JUMP_DURATION = 0.8
+const JUMP_DURATION = 0.5
 const MAX_JUMP_HEIGHT = 5
+
+const GRAVITY = -9.8
 
 var height = 0.0 setget set_height
 var max_air_speed = 0.0
@@ -30,7 +32,7 @@ var velocity = Vector3()
 
 var state = null
 
-enum STATE { IDLE, MOVE, BUMP, JUMP }
+enum STATE { IDLE, MOVE, BUMP, JUMP, IN_AIR }
 
 func _ready():
 	_change_state(IDLE)
@@ -56,8 +58,8 @@ func _change_state(new_state):
 			air_velocity = velocity
 			$AnimationPlayer.play('idle')
 			
-			$Tween.interpolate_method(self, '_animate_jump_height', 0, 1, JUMP_DURATION, Tween.TRANS_LINEAR, Tween.EASE_IN)
-			$Tween.start()
+#			$Tween.interpolate_method(self, '_animate_jump_height', 0, 1, JUMP_DURATION, Tween.TRANS_LINEAR, Tween.EASE_IN)
+#			$Tween.start()
 			
 	state = new_state
 	emit_signal('state_changed', new_state)
@@ -76,49 +78,66 @@ func _physics_process(delta):
 		var collission_info = move(delta)
 		if collission_info:
 			var collider = collission_info.collider
-			if speed == MAX_RUN_SPEED and collider.is_in_group('environment'):
+			if speed == MAX_RUN_SPEED and collider.is_in_group('environment') and is_on_wall():
 				_change_state(BUMP)
 	elif state == JUMP:
 		jump(delta)
+	elif state == IN_AIR and is_on_floor():
+		_change_state(IDLE)
+	elif state == IN_AIR:
+		jump(delta)
+	
+	velocity.y += GRAVITY * delta
+	move_and_slide(velocity, Vector3(0, 1, 0))
 
 func update_direction():
 	if input_direction:
 		last_move_direction = input_direction
 
 func move(delta):
+	
+	var direction = Vector3()
+	
 	if input_direction:
 		if speed != max_speed:
 			speed = max_speed
 	else:
 		speed = 0.0
 	emit_signal('speed_updated', speed)
-		
-	velocity = input_direction.normalized() * speed * delta
-	move_and_slide(velocity, Vector3(0, 1, 0))
+	
+	direction = input_direction.normalized() * speed * delta
+	
+	velocity.x = direction.x
+	velocity.z = direction.z
 	
 	var slide_count = get_slide_count()
 	return get_slide_collision(slide_count - 1) if slide_count else null
 	
 func jump(delta):
-	var AIR_ACCELERATION = 10
-	var AIR_DECCELERATION = 20
-	var AIR_STEERING_POWER = 30
+	var AIR_ACCELERATION = 1
+	var AIR_DECCELERATION = 2
+	var AIR_STEERING_POWER = 3
 	if input_direction:
 		air_speed += AIR_ACCELERATION * delta
 	else:
 		air_speed -= AIR_DECCELERATION * delta
 		
 	air_speed = clamp(air_speed, 0.0, max_air_speed)
+	
 	var target_velocity = air_speed * input_direction.normalized()
 	var steering_velocity = (target_velocity - air_velocity).normalized() * AIR_STEERING_POWER
-	air_velocity += steering_velocity
+#	air_velocity += steering_velocity
+	velocity.x += steering_velocity.x
+	velocity.z += steering_velocity.z
 	
-	move_and_slide(air_velocity)
+	if is_on_floor():
+		velocity.y = 10
+		_change_state(IN_AIR)
 	
 	
 func set_height(value):
-	translation.y = value
 	height = value
+	$Pivot.translation.y = value
 	
 func animate_bump_height(progress):
 	self.height = pow(sin(progress * PI), 0.4) * MAX_BUMP_HEIGHT
